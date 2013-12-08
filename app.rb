7e108d0d52a9a35d413f1ff31e1360af047d6dd0
Spike Grobstein <me@spike.cx>
2013-12-08 08:08:45 -0500

    got working stuff
    namespaces, views, new way of working with processes

--- app.rb
+++ app.rb
@@ -1,26 +1,43 @@
 require 'rubygems'
 require 'bundler/setup'
-require 'pry'
-require './lib/plex'
+
 require 'sinatra/base'
+require 'sinatra/namespace'
+require 'haml'
+
+require 'pry'
+require './lib/home_theater/process'
 
 class PlexDashboardApp < Sinatra::Base
+  register Sinatra::Namespace
   set :bind, '0.0.0.0'
 
-  get '/status' do
-    Plex::MediaServer.running?.to_s
+  namespace '/api' do
+    namespace '/h(ometheater)?' do
+      before do
+        @process = HomeTheater::Process.new('Plex Home Theater.app')
+      end
+      get '/status' do
+        @process.running?.to_s
+      end
+
+      post '/start' do
+        @process.start
+      end
+
+      post '/stop' do
+        @process.stop
+      end
+
+      post '/restart' do
+        @process.restart
+      end
+    end
   end
 
-  post '/start' do
-    Plex::MediaServer.start
-  end
-
-  post '/stop' do
-    Plex::MediaServer.stop
-  end
-
-  post '/restart' do
-    Plex::MediaServer.restart
+  get '/' do
+    @home_theater = HomeTheater::Process.new('Plex Home Theater.app')
+    haml :main
   end
 
   run!
---
+++ lib/home_theater/process.rb
@@ -0,0 +1,55 @@
+require 'cocaine'
+
+module HomeTheater
+  class Process
+    attr_accessor :app_name
+
+    def initialize( app_name )
+      @app_name = app_name
+    end
+
+    # return whether the app is running, based on `pid` return value
+    def running?
+      !!pid
+    end
+
+    # start the app
+    def start
+      `open "/Applications/#{ @app_name }"`
+    end
+
+    # stop the process
+    # returns true if the process was stopped
+    # will kill -9 it if it's still not running
+    def stop
+      return false unless running?
+
+      `kill #{ pid_of_app }`
+
+      running? && `kill -9 #{ pid_of_app }`
+      return true
+    end
+
+    # restart the process
+    # by stopping, then starting it
+    def restart
+      stop
+      start
+    end
+
+    private
+
+    # parse output of `ps aux`
+    # return the pid of this process if it's running
+    # otherwise, return nil
+    def pid
+      line = Cocaine::CommandLine.new('ps', 'aux')
+      line.run.split(/\n/).each do |line|
+        m = line.match(/^.+?\s(\d+)\s.+#{ @app_name }/)
+        return m[1] if m
+      end
+
+      return nil
+    end
+  end
+end
