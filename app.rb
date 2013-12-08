060814ea75feb36f95acfd55321cfbf8eaccfcda
Spike Grobstein <me@spike.cx>
2013-12-08 08:53:02 -0500

    added some customization ability

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ require 'sinatra/base'
 require 'sinatra/namespace'
 require 'haml'
 
+require 'yaml'
+
 require 'pry'
 require './lib/home_theater/process'
 
@@ -13,26 +15,37 @@ class PlexDashboardApp < Sinatra::Base
   set :bind, '0.0.0.0'
 
   namespace '/api' do
-    namespace '/h' do
-      before do
-        @process = HomeTheater::Process.new('Plex Home Theater.app')
-      end
-      get '/status' do
-        @process.running?.to_s
+
+    Dir.glob('./config/*.yml').each do |config|
+      config = YAML.load( File.read(config) )
+
+      next unless config['enabled']
+
+      puts "Got config: #{ config['name'] }"
+
+      namespace "/#{ config['route'] }" do
+        before do
+          @process = HomeTheater::Process.new(config['app_name'])
+        end
+        get '/status' do
+          @process.running?.to_s
+        end
+
+        post '/start' do
+          @process.start
+        end
+
+        post '/stop' do
+          @process.stop
+        end
+
+        post '/restart' do
+          @process.restart
+        end
       end
 
-      post '/start' do
-        @process.start
-      end
-
-      post '/stop' do
-        @process.stop
-      end
-
-      post '/restart' do
-        @process.restart
-      end
     end
+
   end
 
   get '/' do
