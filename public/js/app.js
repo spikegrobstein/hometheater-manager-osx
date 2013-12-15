40496216467c0718b46438f053e34ce9379e569a
Spike Grobstein <me@spike.cx>
2013-12-14 21:04:40 -0500

    added and implemented refresh button

--- public/js/app.js
+++ public/js/app.js
@@ -1,33 +1,32 @@
-$(function() {
-
-  // ensure that a button's class is correct
-  // based on its action
-  function update_button_content( element ) {
-    $.ajax({
-      type: 'GET',
-      url: '/api/' + $(element).data('route') + '/',
-      success: function( data ) {
-        if ( data === 'true' ) {
-          // it's running
-          // class: btn-success
-          // action: stop
-          $(element)
-            .removeClass('btn-danger')
-            .addClass('btn-success')
-            .data('action', 'stop');
-        } else {
-          // it's not running
-          // class: btn-success
-          // action: stop
-          $(element)
-            .removeClass('btn-success')
-            .addClass('btn-danger')
-            .data('action', 'start');
-        }
+// ensure that a button's class is correct
+// based on its action
+function update_button_content( element ) {
+  $.ajax({
+    type: 'GET',
+    url: '/api/' + $(element).data('route') + '/',
+    success: function( data ) {
+      if ( data === 'true' ) {
+        // it's running
+        // class: btn-success
+        // action: stop
+        $(element)
+          .removeClass('btn-danger')
+          .addClass('btn-success')
+          .data('action', 'stop');
+      } else {
+        // it's not running
+        // class: btn-success
+        // action: stop
+        $(element)
+          .removeClass('btn-success')
+          .addClass('btn-danger')
+          .data('action', 'start');
       }
-    })
-  }
+    }
+  })
+}
 
+function update_all_buttons() {
   var buttons = $('.service-button');
 
   buttons.each(function(i, button) {
@@ -54,6 +53,11 @@ $(function() {
 
       return false;
     });
+}
 
+$(function() {
+  update_all_buttons();
+
+  $('#refresh-button').click(update_all_buttons);
 });
 
